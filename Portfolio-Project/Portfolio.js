ad98df33509355204a945b6ae616ed44daeb2fd5
Diabolica <scripter.Diabolica@gmail.com>
2021-10-09 19:31:38 +0300

    Finished Loading Screen, and edits to cards

--- Portfolio-Project/Portfolio.js
+++ Portfolio-Project/Portfolio.js
@@ -18,4 +18,33 @@ async function createBackgroundEffect() {
   }
 }
 
-createBackgroundEffect();
+async function createLoadingEffect(){
+  let bubbleStorage = document.getElementById("bubbleStorage");
+  for(let i = 0; i < 1000; i++) {
+    let element = document.createElement("span");
+    element.className = "loading-bubble";
+    let size = 20 + Math.random() * 60;
+
+    element.style.width = size + "px";
+    element.style.height = size + "px";
+    element.style.left = (Math.random() * document.documentElement.clientWidth).toString() + "px";
+    bubbleStorage.appendChild(element);
+
+    setTimeout(() => element.remove(), 4000);
+    await sleep(1);
+  }
+}
+
+async function loadWebsite(){
+  createLoadingEffect();
+  await sleep(1150);
+  createBackgroundEffect();
+  let bubbleStorage = document.getElementById("bubbleStorage");
+  for(let i = 250; i > 0; i--){
+    bubbleStorage.style.opacity = i/250
+    await sleep(0.5)
+  }
+  bubbleStorage.remove();
+}
+
+loadWebsite();
