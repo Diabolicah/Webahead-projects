cabcdc61db182befdfd9352ed3968f2b5994d233
Diabolica <scripter.Diabolica@gmail.com>
2021-10-12 15:32:19 +0300

    Finished Calculator

--- Calculator-Project/Calculator.js
+++ Calculator-Project/Calculator.js
@@ -8,10 +8,8 @@ async function createBackgroundEffect() {
     let element = document.createElement("div");
     element.className = "geoObj";
     objStorage.appendChild(element);
-    element.style.top =
-      (Math.random() * document.documentElement.clientHeight).toString() + "px";
-    element.style.left =
-      (Math.random() * document.documentElement.clientWidth).toString() + "px";
+    element.style.top = (Math.random() * document.documentElement.clientHeight).toString() + "px";
+    element.style.left = (Math.random() * document.documentElement.clientWidth).toString() + "px";
     element.style.borderRadius = Math.random() * 100 + "%";
     setTimeout(() => element.remove(), 10000);
     await sleep(1000);
@@ -25,16 +23,67 @@ async function loadWebsite() {
 loadWebsite();
 
 // Calculator
+let operatorsList = ["*", "/", "-", "+"];
 let expressionWin = document.querySelector(".expressionWindow");
 let outputWin = document.querySelector(".outputWindow");
-let operatorActive = false;
+let needOperator = false;
 
 let rstButton = document.querySelector(".resetButton");
+let delButton = document.querySelector(".deleteButton");
+
+let numberButtons = Array.from(document.querySelectorAll(".number"));
+let operatorButtons = Array.from(document.querySelectorAll(".operator"));
 
 rstButton.addEventListener("click", () => {
-  operatorActive = false;
   expressionWin.textContent = "";
-  outputWin.textContent = "";
+  outputWin.textContent = "0";
 });
 
-console.log(eval("2/+5"));
+delButton.addEventListener("click", (event) => {
+  if (outputWin.textContent != "0") {
+    outputWin.textContent = "0";
+    return;
+  }
+  if (operatorsList.includes(expressionWin.textContent.at(-2))) {
+    expressionWin.textContent = expressionWin.textContent.substring(0, expressionWin.textContent.length - 3);
+    needOperator = true;
+  }
+});
+
+numberButtons.forEach((value) => {
+  value.addEventListener("click", (event) => {
+    if (needOperator) return;
+    if (value.classList.contains("dot") && outputWin.textContent.includes(".")) return;
+    if (event.target.textContent == "0" && outputWin.textContent == "0") return;
+    if (outputWin.textContent == "0" && !event.target.classList.contains("dot")) outputWin.textContent = "";
+    if (outputWin.textContent == "-0" && !event.target.classList.contains("dot")) outputWin.textContent = "-";
+    outputWin.textContent += event.target.textContent;
+  });
+});
+
+operatorButtons.forEach((value) => {
+  value.addEventListener("click", (event) => {
+    if (operatorsList.includes(event.target.textContent)) {
+      if (needOperator) {
+        needOperator = false;
+        expressionWin.textContent += " $s ".replace("$s", event.target.textContent);
+        return;
+      }
+      outputWin.textContent += " $s ".replace("$s", event.target.textContent);
+      expressionWin.textContent += outputWin.textContent;
+      outputWin.textContent = "0";
+      return;
+    }
+    if (event.target.classList.contains("negative")) {
+      if (outputWin.textContent[0] == "-") outputWin.textContent = outputWin.textContent.substring(1);
+      else outputWin.textContent = "-" + outputWin.textContent;
+      return;
+    }
+    if (event.target.classList.contains("equal")) {
+      if (expressionWin.textContent.at(-1) == " " && !needOperator) expressionWin.textContent += outputWin.textContent;
+      let evaluation = eval(expressionWin.textContent);
+      expressionWin.textContent = "";
+      outputWin.textContent = evaluation || outputWin.textContent;
+    }
+  });
+});
