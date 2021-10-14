0185d45c0ede4ac80fcc5b3280d05c23f9992f4f
Diabolica <scripter.Diabolica@gmail.com>
2021-10-14 12:20:25 +0300

    Finished Portfolio Completely

--- Portfolio-Project/Portfolio.js
+++ Portfolio-Project/Portfolio.js
@@ -8,19 +8,17 @@ async function createBackgroundEffect() {
     let element = document.createElement("div");
     element.className = "geoObj";
     objStorage.appendChild(element);
-    element.style.top =
-      (Math.random() * document.documentElement.clientHeight).toString() + "px";
-    element.style.left =
-      (Math.random() * document.documentElement.clientWidth).toString() + "px";
+    element.style.top = (Math.random() * document.documentElement.clientHeight).toString() + "px";
+    element.style.left = (Math.random() * document.documentElement.clientWidth).toString() + "px";
     element.style.borderRadius = Math.random() * 100 + "%";
     setTimeout(() => element.remove(), 10000);
     await sleep(1000);
   }
 }
 
-async function createLoadingEffect(){
+async function createLoadingEffect() {
   let bubbleStorage = document.getElementById("bubbleStorage");
-  for(let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 1000; i++) {
     let element = document.createElement("span");
     element.className = "loading-bubble";
     let size = 20 + Math.random() * 60;
@@ -35,16 +33,55 @@ async function createLoadingEffect(){
   }
 }
 
-async function loadWebsite(){
+async function loadWebsite() {
   createLoadingEffect();
   await sleep(1150);
   createBackgroundEffect();
   let bubbleStorage = document.getElementById("bubbleStorage");
-  for(let i = 250; i > 0; i--){
-    bubbleStorage.style.opacity = i/250
-    await sleep(0.5)
+  for (let i = 250; i > 0; i--) {
+    bubbleStorage.style.opacity = i / 250;
+    await sleep(0.25);
   }
   bubbleStorage.remove();
 }
+
+let bigCard = document.querySelector(".big-zoomed-card");
+let isBigCardOpen = false;
+let exitIcon = document.querySelector(".exit-icon");
+
+async function closeBigCard(event) {
+  for (let i = 100; i > 0; i--) {
+    bigCard.style.opacity = i / 100;
+    await sleep(0.25);
+  }
+  Array.from(bigCard.children).forEach((elem) => {
+    if (!elem.classList.contains("exit-icon")) elem.remove();
+  });
+  bigCard.style["z-index"] = "-1";
+  isBigCardOpen = false;
+}
+
+document.getElementById("geoObjStorage").addEventListener("click", closeBigCard);
+exitIcon.addEventListener("click", closeBigCard);
+
+async function openBigCard() {
+  isBigCardOpen = true;
+  Array.from(this.children).forEach((child) => {
+    let tempClone = child.cloneNode(true);
+    tempClone.style.fontSize = "2.25vw";
+    bigCard.appendChild(tempClone);
+  });
+  bigCard.style["z-index"] = "1";
+  for (let i = 1; i <= 100; i++) {
+    bigCard.style.opacity = i / 100;
+    await sleep(0.25);
+  }
+}
+
+let backCards = Array.from(document.querySelectorAll(".back"));
+
+backCards.forEach((elem) => {
+  elem.addEventListener("click", openBigCard);
+});
 
 loadWebsite();
