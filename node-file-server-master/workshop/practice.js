3dbbc6bbd448335103b6ec645889390c53d3de13
Diabolica <scripter.Diabolica@gmail.com>
2021-10-31 15:46:35 +0200

    Workshop done

--- node-file-server-master/workshop/practice.js
+++ node-file-server-master/workshop/practice.js
@@ -1,7 +1,12 @@
-console.log("try out the fs module here");
-
 const fs = require("fs");
+const path = require("path");
 
-fs.readFile("workshop/test.txt", (error, file) => {
-  console.log(error, file);
+console.log(__dirname);
+
+fs.readFile(path.join(__dirname, "test.txt"), { encoding: "utf-8" }, (error, file) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(file);
 });
